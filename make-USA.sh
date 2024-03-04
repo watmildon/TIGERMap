8cab4559b302e01337d980c2c3e298f6f3d68540
Matt Whilden <118567155+watmildon@users.noreply.github.com>
2024-03-04 14:56:10 -0800

    drop in tile making script

---
+++ make-USA.sh
@@ -0,0 +1,22 @@
+cd "$(dirname "$0")";
+rm *.pmtiles
+rm *.pmtiles-journal
+rm *.geojson
+rm us-latest-addrstreet.osm.pbf
+rm us-latest-tiger.osm.pbf
+
+rm tilesets/us-latest.pmtiles
+rm tilesets/us-latest-streetaddress.pmtiles
+
+osmium tags-filter --remove-tags --overwrite us-latest.osm.pbf w/tiger:reviewed=no -o us-latest-tiger.osm.pbf
+osmium export --overwrite us-latest-tiger.osm.pbf  -o us-latest-tiger.geojson
+tippecanoe -zg -l highways -o us-latest.pmtiles --drop-densest-as-needed us-latest-tiger.geojson
+
+osmium tags-filter --remove-tags --overwrite us-latest.osm.pbf nwr/addr:street -o us-latest-addrstreet.osm.pbf
+osmium export --overwrite us-latest-addrstreet.osm.pbf -o us-latest-addrstreet.geojson
+tippecanoe -zg -l streetaddress -o us-latest-streetaddress.pmtiles --drop-densest-as-needed us-latest-addrstreet.geojson
+
+mv us-latest.pmtiles tilesets/us-latest.pmtiles
+mv us-latest-streetaddress.pmtiles tilesets/us-latest-streetaddress.pmtiles
+
+rclone sync --transfers 1 --order-by size,descending --bwlimit 10M ~/TIGERProject/tilesets r2:tiger-map
