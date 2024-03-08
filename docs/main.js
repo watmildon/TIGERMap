3952c27452730a570776779f99b230cbaa7a7126
Matt Whilden <118567155+watmildon@users.noreply.github.com>
2024-03-07 18:30:46 -0800

    Improve options in the filter box

--- docs/main.js
+++ docs/main.js
@@ -10,12 +10,51 @@ function updateMapLayers(checkboxId,layerName) {
 
 function filterMap() {
     var filterTextBox = document.getElementById("filterTextBox");
+    var filterText = filterTextBox.value;
     
-    var kvp = filterTextBox.value.split("=");
-
-    window.tigerMap.setFilter('tigerReview', ['all', ['==', kvp[0], kvp[1]]]);
-
-    window.tigerMap.setLayoutProperty('tigerReview', 'visibility', 'visible');
+    if (filterText.includes('='))
+    {
+        var kvp = filterText.split("=");
+        if (filterText.includes(','))
+        {
+            // surface=paved,asphalt
+            // "filter": ["match", ["get", "surface"], ["paved", "asphalt"], true, false]
+            var values = kvp[1].split(',');
+            
+            if (values.length == 2)
+            {
+                window.tigerMap.setFilter('tigerReview', ['match', ['get', kvp[0]], [values[0],values[1]],true,false]);
+            }
+            if (values.length == 3)
+            {
+                window.tigerMap.setFilter('tigerReview', ['match', ['get', kvp[0]], [values[0],values[1],values[2]],true,false]);
+            }
+            if (values.length == 4)
+            {
+                window.tigerMap.setFilter('tigerReview', ['match', ['get', kvp[0]], [values[0],values[1],values[2],values[3]],true,false]);
+            }
+            if (values.length == 5)
+            {
+                window.tigerMap.setFilter('tigerReview', ['match', ['get', kvp[0]], [values[0],values[1],values[2],values[3],values[4]],true,false]);
+            }
+        }
+        else
+        {
+            if (kvp[1] = "*")
+            {
+                window.tigerMap.setFilter('tigerReview', ["has",kvp[0]]);
+            }
+            else
+            {
+                window.tigerMap.setFilter('tigerReview', ['all', ['==', kvp[0], kvp[1]]]);
+            }
+            window.tigerMap.setLayoutProperty('tigerReview', 'visibility', 'visible');
+        }
+    }
+    else
+    {
+        window.tigerMap.setFilter('tigerReview', ["has",filterText]);
+    }
   }
 
   function clearFilter() {
