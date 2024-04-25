89a071c4772f7386dff24a78f7e26d2e993b0960
Matt Whilden <118567155+watmildon@users.noreply.github.com>
2024-04-24 21:14:27 -0700

    Include OSM meta tags

--- build-tiles.sh
+++ build-tiles.sh
@@ -31,17 +31,17 @@ pyosmium-up-to-date -v --server https://download.geofabrik.de/north-america/us/w
 pyosmium-up-to-date -v --server https://download.geofabrik.de/north-america/us/utah-updates/ -s 10000 utah-latest.osm.pbf
 
 osmium tags-filter --remove-tags --overwrite us-latest.osm.pbf w/tiger:reviewed=no -o us-latest-tiger.osm.pbf
-osmium export --overwrite us-latest-tiger.osm.pbf  -o us-latest-tiger.geojson
+osmium export --attributes type,id,version,timestamp --overwrite us-latest-tiger.osm.pbf -o us-latest-tiger.geojson
 tippecanoe -zg -l highways -o us-latest.pmtiles --drop-densest-as-needed us-latest-tiger.geojson
 
 osmium tags-filter --remove-tags --overwrite us-latest.osm.pbf nwr/addr:street -o us-latest-addrstreet.osm.pbf
 osmium export --overwrite us-latest-addrstreet.osm.pbf -o us-latest-addrstreet.geojson
 tippecanoe -zg -l streetaddress -o us-latest-streetaddress.pmtiles --drop-densest-as-needed us-latest-addrstreet.geojson
 
-osmium export washington-latest.osm.pbf -o washington-latest.geojson
+osmium export --attributes type,id,version,timestamp washington-latest.osm.pbf -o washington-latest.geojson
 tippecanoe -zg -l allFeatures -o washington-latest.pmtiles --drop-densest-as-needed washington-latest.geojson
 
-osmium export utah-latest.osm.pbf -o utah-latest.geojson
+osmium export --attributes type,id,version,timestamp utah-latest.osm.pbf -o utah-latest.geojson
 tippecanoe -zg -l allFeatures -o utah-latest.pmtiles --drop-densest-as-needed utah-latest.geojson
 
 mv us-latest.pmtiles tilesets/us-latest.pmtiles
